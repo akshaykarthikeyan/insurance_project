f579f15bf1d1b58786e6b206b4a738c542d5abeb
akshaykarthikeyan <akshaykarthikeyan4@gmail.com>
2022-02-22 11:19:32 +0530

    Update setting.dart

--- lib/screens/setting.dart
+++ lib/screens/setting.dart
@@ -446,7 +446,7 @@ class _SettingState extends State<Setting> {
                                                 obscureText: true,
                                                 decoration: InputDecoration(
                                                     suffixIcon: GestureDetector(
-                                                      onTap: () {
+                                                      onTap: () async {
                                                         setState(() {
                                                           _isObscure =
                                                               !_isObscure;
